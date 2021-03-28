63cd589ddd179d7ff65b73f9973c6b83ba4074df
kianho <kianho@users.noreply.github.com>
2021-03-28 16:11:42 +1100

    Temporarily removed the f_handler to avoid writing log files where permissions are unknown

--- neuralprophet/__init__.py
+++ neuralprophet/__init__.py
@@ -4,22 +4,22 @@ log = logging.getLogger("NP")
 log.setLevel("INFO")
 # Create handlers
 c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler("logs.log", "w+")
+#f_handler = logging.FileHandler("logs.log", "w+")
 # c_handler.setLevel("WARNING")
 # f_handler.setLevel("INFO")
 # Create formatters and add it to handlers
 c_format = logging.Formatter("%(levelname)s - (%(name)s.%(funcName)s) - %(message)s")
 f_format = logging.Formatter("%(asctime)s; %(levelname)s; %(name)s; %(funcName)s; %(message)s")
 c_handler.setFormatter(c_format)
-f_handler.setFormatter(f_format)
+#f_handler.setFormatter(f_format)
 # Add handlers to the logger
 log.addHandler(c_handler)
-log.addHandler(f_handler)
+#log.addHandler(f_handler)
 
 logging.captureWarnings(True)
 warnings_log = logging.getLogger("py.warnings")
 warnings_log.addHandler(c_handler)
-warnings_log.addHandler(f_handler)
+#warnings_log.addHandler(f_handler)
 
 from .forecaster import NeuralProphet
 from .utils import set_random_seed, set_log_level
